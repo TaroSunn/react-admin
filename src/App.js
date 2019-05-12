f82098e585303a06e3c44b41bce4ad2387ef9007
tarosun <sunxiaomingyes@gmail.com>
2019-05-12 21:10:27 +0800

    study

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Life from './pages/demo/Life'
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Life></Life>
       </header>
     </div>
   );
