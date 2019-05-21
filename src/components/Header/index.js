b842d2fe9369cde46c426e4334566aba225810cb
tarosun <sunxiaomingyes@gmail.com>
2019-05-21 22:18:42 +0800

    首页天气

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd'
 import Util from './../../util/utils'
 import './index.less'
-
+import axios from 'axios'
 export default class Header extends Component {
   componentWillMount() {
     this.setState({
@@ -15,7 +15,22 @@ export default class Header extends Component {
         sysTime
       })
     },1000)
-    
+    this.getWeatherAPIData()
+  }
+  async getWeatherAPIData() {
+    let [res] = await Promise.all([
+      axios.get('https://www.tianqiapi.com/api/?version=v1')
+    ])
+    if(res.status===200) {
+      let data = res.data.data[0]
+      this.setState({
+        weaType: data.wea, // 天气状态
+        maxTemp: data.tem1,
+        minTemp: data.tem2,
+      })
+    } else {
+      console.log('error')
+    }
   }
   render() {
     return (
@@ -32,7 +47,7 @@ export default class Header extends Component {
           </Col>
           <Col span="20" className="weather">
             <span className="date">{this.state.sysTime}</span>
-            <span className="weather-detail">晴</span>
+            <span className="weather-detail">{this.state.weaType} {this.state.minTemp}-{this.state.maxTemp}</span>
           </Col>
         </Row>
       </div>
